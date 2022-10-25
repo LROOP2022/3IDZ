4e4ea5a50a39e1788b2373574b86ac8e909598b1
yaolg <yaolg@DESKTOP-PS4EPC5>
2022-10-25 20:21:19 +0300

    Добавьте файлы проекта.

---
+++ ЛР3ИДЗ/Program.cs
@@ -0,0 +1,20 @@
+﻿using Matrixx;
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix= new Matrix(3,3);
+            Matrix.PrintMatrix(matrix);
+            float []m=Matrix.GetSrArif(matrix);
+            foreach (int i in m)
+            {
+                Console.Write(" "+i);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Matrix.PrintResultMatrix(matrix);
+        }
+    }
+}
